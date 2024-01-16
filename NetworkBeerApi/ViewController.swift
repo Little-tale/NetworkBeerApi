89534230dab191654d14970df808227f512ecf4e
judge_happily <you3192001@gmail.com>
2024-01-16 18:24:08 +0900

    Initial Commit

---
+++ NetworkBeerApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkBeerApi
+//
+//  Created by Jae hyung Kim on 1/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
